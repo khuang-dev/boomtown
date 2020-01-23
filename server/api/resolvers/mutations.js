77c78552b1e7ec2d7f1812449af20c837ccf8b5d
Ken <khuang.dev@gmail.com>
2020-01-23 12:03:23 -0800

    add deconstruct to addItem mutation.js

--- server/api/resolvers/mutations.js
+++ server/api/resolvers/mutations.js
@@ -135,20 +135,10 @@ const mutationResolvers = app => ({
     return true;
   },
   async addItem(parent, args, context, info) {
-    /**
-     *  @TODO: Destructuring
-     *
-     *  The 'args' and 'context' parameters of this resolver can be destructured
-     *  to make things more readable and avoid duplication.
-     *
-     *  When you're finished with this resolver, destructure all necessary
-     *  parameters in all of your resolver functions.
-     *
-     *  Again, you may look at the user resolver for an example of what
-     *  destructuring should look like.
-     */
+    const {title, description, tags} = args.item;
+    const { pgResource } = context;
     const user = await jwt.decode(context.token, app.get("JWT_SECRET"));
-    const newItem = await context.pgResource.saveNewItem({
+    const newItem = await pgResource.saveNewItem({
       item: args.item,
       user,
     });
