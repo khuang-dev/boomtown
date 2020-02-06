6d9e437731a613d949ee6dbaad466153a03a75af
Ken <khuang.dev@gmail.com>
2020-02-06 09:16:13 -0800

    update profile container

--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Profile from './Profile';
 // import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import {  } from '../../apollo/queries';
+import { Query } from 'react-apollo';
+import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
 
 class ProfileContainer extends Component {
   render() {
